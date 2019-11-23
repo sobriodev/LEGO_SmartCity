30183fad5f1f18bde62f0302431781df008b0f2d
Mateusz Kiełbasa <sobriodev@gmail.com>
2019-11-23 10:45:02 +0100

    Added JSON error response

--- source/httpsrv/api.c
+++ source/httpsrv/api.c
@@ -1,5 +1,16 @@
 #include "api.h"
 #include "query_string.h"
+#include "cJSON.h"
+
+#include "fsl_debug_console.h"
+
+/* ----------------------------------------------------------------------------- */
+/* ------------------------------- PRIVATE MACROS ------------------------------ */
+/* ----------------------------------------------------------------------------- */
+
+/* JSON error */
+#define HTTPSRV_JSON_ERR_MAX_LEN	200
+#define HTTPSRV_JSON_ERR 			"{\"statusCode\": %d, \"message\": \"%s\"}"
 
 /* ----------------------------------------------------------------------------- */
 /* ------------------------------- PUBLIC VARIABLES ---------------------------- */
@@ -11,6 +22,24 @@ const HTTPSRV_CGI_LINK_STRUCT HTTPSRV_ApiTable[] = {
 };
 
 /* ----------------------------------------------------------------------------- */
+/* ------------------------------ PRIVATE FUNCTIONS ---------------------------- */
+/* ----------------------------------------------------------------------------- */
+
+/* Return specific HTTP error code */
+static void HTTPSRV_MakeJsonError(HTTPSRV_CGI_RES_STRUCT *response, uint32_t statusCode, const char *msg)
+{
+	/* cJSON is not used here for better performance */
+	static char jsonResponse[HTTPSRV_JSON_ERR_MAX_LEN];
+	snprintf(jsonResponse, HTTPSRV_JSON_ERR_MAX_LEN, HTTPSRV_JSON_ERR, statusCode, msg);
+
+	response->status_code = statusCode;
+	response->content_type = HTTPSRV_CONTENT_TYPE_JSON;
+	response->data = jsonResponse;
+	response->data_length = strlen(jsonResponse);
+	response->content_length = response->data_length;
+}
+
+/* ----------------------------------------------------------------------------- */
 /* -------------------------------- API FUNCTIONS ------------------------------ */
 /* ----------------------------------------------------------------------------- */
 
@@ -18,8 +47,6 @@ int32_t API_TestConn(HTTPSRV_CGI_REQ_STRUCT *param)
 {
 	HTTPSRV_CGI_RES_STRUCT response = {0};
     response.ses_handle = param->ses_handle;
-    response.status_code = HTTPSRV_CODE_OK;
-    response.content_type = HTTPSRV_CONTENT_TYPE_JSON;
 
 	QS_Param_t params[2];
 	uint8_t paramsFound = QS_Parse(param->query_string, params, 2);
@@ -27,13 +54,13 @@ int32_t API_TestConn(HTTPSRV_CGI_REQ_STRUCT *param)
 	const QS_Param_t *led = QS_GetParam("led", params, paramsFound);
 
 	if (QS_PARAM_EXISTS(home) && QS_PARAM_EXISTS(led) && QS_PARAM_IS_INT(home) && QS_PARAM_IS_BOOL(led)) {
-	    response.data = "{response: \"OK\"}";
-	    response.data_length = strlen("{response: \"OK\"}");
+		response.status_code = 200;
+		response.content_type = HTTPSRV_CONTENT_TYPE_JSON;
+	    response.data = "{\"response\": \"OK\"}";
+	    response.data_length = strlen("{\"response\": \"OK\"}");
 	    response.content_length = response.data_length;
 	} else {
-	    response.data = "{response: \"ER\"}";
-	    response.data_length = strlen("{response: \"ER\"}");
-	    response.content_length = response.data_length;
+		HTTPSRV_MakeJsonError(&response, 404, "LED not found!");
 	}
 
     HTTPSRV_cgi_write(&response);
